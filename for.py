f874ecde736553b4e67fbf0742f67ced51477cdf
Rodrigo Quispe <rquispe22@autonoma.edu.pe>
2025-09-09 13:57:11 -0500

    add file control flow loop

---
+++ for.py
@@ -0,0 +1,9 @@
+# file title: for.py
+# description: estructura de control for
+# datetime created: 09/09/2025 13:50
+# author: cusco
+
+fruits = ["manzana", "banana", "naranja"]
+
+for a_fruit in fruits:
+    print(a_fruit)
---
+++ while.py
@@ -0,0 +1,11 @@
+'''
+    file title: while.py
+    description: estructura de buble while
+    datetime: 09/09/2025 13:54
+    autor: cusco
+'''
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
